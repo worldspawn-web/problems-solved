29b7b3ff11c01ad88af8f4cb5005782cf1629e7f
Worldspawn <worldspawn.web@gmail.com>
2023-07-31 17:15:18 +0300

    LC: 2665

---
+++ leetcode/Easy/2665_Counter_II.js
@@ -0,0 +1,25 @@
+class Counter {
+  constructor(init) {
+    this.init = init;
+    this.presentCount = init;
+  }
+
+  increment() {
+    this.presentCount += 1;
+    return this.presentCount;
+  }
+
+  decrement() {
+    this.presentCount -= 1;
+    return this.presentCount;
+  }
+
+  reset() {
+    this.presentCount = this.init;
+    return this.presentCount;
+  }
+}
+
+const createCounter = (init) => {
+  return new Counter(init);
+};
