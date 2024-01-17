b86988c0668686551222d8f1a8f7eda0c4b158aa
Worldspawn <worldspawn.web@gmail.com>
2024-01-17 18:45:33 +0300

    fix: fixed annoying ts bug, which is unsolvable by restarting TS server

--- leetcode/typescript/Easy/88_Merge_Sorted_Array.ts
+++ leetcode/typescript/Easy/88_Merge_Sorted_Array.ts
@@ -5,13 +5,13 @@ interface MergeArrays {
   (nums1: number[], m: number, nums2: number[], n: number): void;
 }
 
-const merge: MergeArrays = (nums1, m, nums2, n) => {
+const mergeTs: MergeArrays = (nums1, m, nums2, n) => {
   nums1.length = m;
   nums1.push(...nums2);
   nums1.sort((a, b) => a - b);
 };
 
 // Test Cases:
-const test1 = merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3); // Expected: nums1 = [1,2,2,3,5,6]
-const test2 = merge([1], 1, [], 0); // Expected: nums1 = [1]
-const test3 = merge([0], 0, [1], 1); // Expected: nums1 = [1]
+const testN1 = mergeTs([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3); // Expected: nums1 = [1,2,2,3,5,6]
+const testN2 = mergeTs([1], 1, [], 0); // Expected: nums1 = [1]
+const testN3 = mergeTs([0], 0, [1], 1); // Expected: nums1 = [1]
